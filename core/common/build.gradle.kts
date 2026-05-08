1f2b8baed21e8931de67c20a685d562c56e730be
Giuseppe Tamayo Cañizares <gtamayoc@ufpso.edu.co>
2026-05-08 17:57:53 -0500

    fix(core/common): properly remove iOS block and close kotlin
    The core/common module had iosArm64Main and iosSimulatorArm64Main
    get declarations that were missed in the bulk fix.
    Fixed to match all other modules structure.

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -52,14 +52,7 @@ kotlin {
         val androidMain by getting
         val desktopMain by getting
         val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-        }
+    }
 }
 
 android {
