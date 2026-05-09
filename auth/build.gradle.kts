32d02af1d282006d9c239796a4af1577224ca7f6
Giuseppe Tamayo Cañizares <gtamayoc@ufpso.edu.co>
2026-05-08 19:13:59 -0500

    fix(auth): add ktor-client-auth dependency

--- auth/build.gradle.kts
+++ auth/build.gradle.kts
@@ -26,6 +26,7 @@ kotlin {
                 implementation(libs.ktor.client.core)
                 implementation(libs.ktor.client.content.negotiation)
                 implementation(libs.ktor.serialization.json)
+                implementation(libs.ktor.client.auth)
                 implementation(libs.multiplatform.settings)
                 implementation(libs.napier)
             }
